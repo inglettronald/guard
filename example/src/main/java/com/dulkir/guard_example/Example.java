361db80cd3defc8e69825a20237c38960b0c5602
inglettronald <inglettronald@gmail.com>
2024-12-07 19:06:08 -0600

    add example code for evaluating @Guard.Null base case

--- example/src/main/java/com/dulkir/guard_example/Example.java
+++ example/src/main/java/com/dulkir/guard_example/Example.java
@@ -1,9 +1,15 @@
 package com.dulkir.guard_example;
 
+import com.dulkir.guard.Guard;
+
 public class Example {
 
     public static void main(String[] args) {
         System.out.println("Inside Example.main");
+
+        @Guard.Null String foo = null;
+
+        System.out.println("We did not early return.");
     }
 
 }
